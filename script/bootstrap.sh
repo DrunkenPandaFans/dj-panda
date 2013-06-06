5b3af754a308e46ad590d4076e8f9cc9ab9fceca
Jan Ferko <julyloov@gmail.com>
2013-06-07 00:20:48 +0200

    ** Initial bootstrap script

---
+++ script/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# Script to setup your ultimate jukebox.
+#
+
+echo "Welcome to DJ Panda."
+echo "Playing only the best music."
+echo "Sponsored by nobody. :)"
+echo ""
+
+echo "But first you have to do little bit of work."
+echo ""
+
+#
+# Do some magic setup here.
+#
+
+#
+# Good job. Panda is ready to play.
+#
+echo "Good Job! Panda is ready."
