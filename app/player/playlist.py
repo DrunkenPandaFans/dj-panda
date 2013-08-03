a435f73cc1c754b5b3654aa3bb1d44077793327f
paynes <jan.koscak@gmail.com>
2013-08-03 14:32:24 +0200

    exception changed and next_song modified

--- app/player/playlist.py
+++ app/player/playlist.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-class WrongSongException(Exception):
+class PlaylistException(Exception):
     pass
 
 
@@ -11,28 +11,28 @@ class Playlist(object):
         self._playlist = []
         self._current_song = None
 
-    def add_song(self, song):
-        if not song:
-            raise WrongSongException
+    def add_song(self, song_path):
+        if not song_path:
+            raise PlaylistException("Song path is empty.")
 
         if not self.playlist:
-            self._current_song = song
-            self._playlist.append(song)
+            self._current_song = song_path
+            self._playlist.append(song_path)
         else:
-            self._playlist.append(song)
+            self._playlist.append(song_path)
 
-    def add_song_to_top(self, song):
-        if not song:
-            raise WrongSongException
+    def add_song_to_top(self, song_path):
+        if not song_path:
+            raise PlaylistException("Song path is empty.")
 
-        self._playlist.insert(0, song)
-        self._current_song = song
+        self._playlist.insert(0, song_path)
+        self._current_song = song_path
 
-    def add_song_to_index(self, i, song):
-        if not song:
-            raise WrongSongException
+    def add_song_to_index(self, i, song_path):
+        if not song_path:
+            raise PlaylistException("Song path is empty.")
 
-        self._playlist.insert(i, song)
+        self._playlist.insert(i, song_path)
 
     def set_current_song_to_index(self, i):
         song = self.playlist[i]
@@ -50,6 +50,10 @@ class Playlist(object):
     def next_song(self):
         if self._playlist:
             self._playlist.pop(0)
-            self._current_song = self.playlist[0]
+            try:
+                self._current_song = self.playlist[0]
+            except (IndexError):
+                self._current_song = None
+                raise PlaylistException("Playlist is empty")
         else:
             self._current_song = None
