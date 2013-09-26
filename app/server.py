d80b292f3e9d90fec411f08b5f0fa3aa2879ec4b
Jan Ferko <julyloov@gmail.com>
2013-09-26 15:15:34 +0200

    Added saving access tokens to mongo

--- app/server.py
+++ app/server.py
@@ -2,7 +2,7 @@
 
 from model.user import UserDAO
 from util.github import GitHub, GitHubAuthenticator
-from bottle import redirect, request, route, run
+from bottle import redirect, request, response, route, run
 from pymongo import MongoClient
 
 import json
@@ -18,7 +18,15 @@ def login_callback():
   code = request.query['code']
   token = AUTHENTICATOR.retrieve_access_token(code)
   user = GITHUB_API.get_user_info(token)
+  orgs = GITHUB_API.get_user_orgs(token)
+  for org in orgs:
+    if org['login'] == CONFIG['github_org']:
+      USER_DAO.save(user['login'], token)
+      response.set_cookie('login', user['login'])
+      break
+
   return "Hello %s!! Your access token is %s" % (user["login"], token)
+
 
 def load_config():
   config_file = open('config/config.json')
@@ -40,4 +48,4 @@ client = MongoClient(CONFIG['mongo_url'], CONFIG['mongo_port'])
 USER_DAO = UserDAO(client.test)
 AUTHENTICATOR = prepare_authenticator()
 GITHUB_API = GitHub(CONFIG['github_client_id'], CONFIG['github_client_secret'])
-run(host='localhost', port='8080')
+run(host='localhost', port='9080')
